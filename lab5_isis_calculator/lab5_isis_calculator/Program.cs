5a6535420960dc89ea69e546030a69d089aeb7ad
Andrey Cherepanov <unfuer@gmail.com>
2023-05-28 20:20:15 +0300

    Третий пуш: сделал шапку программы

--- lab5_isis_calculator/lab5_isis_calculator/Program.cs
+++ lab5_isis_calculator/lab5_isis_calculator/Program.cs
@@ -13,10 +13,12 @@ namespace lab5_isis_calculator
         {
             int a, b;
             double c;
+            Console.WriteLine("\nК О Н С О Л Ь Н О Е   П Р И Л О Ж Е Н И Е   К А Л Ь К У Л Я Т О Р\nversion 0.1   developed by andrey cherepanov\n");
             Console.Write("Первое число: "); a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Второе число: "); b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("\n\nВыберите действие:\n1. Сложить\n2. Вычесть\n3. Умножить\n4. Разделить\n\nДействие: "); c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nВторое число: "); b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВыберите действие:\n1. Сложить\n2. Вычесть\n3. Умножить\n4. Разделить\n\nДействие: "); c = Convert.ToDouble(Console.ReadLine());
             Console.Clear();
+            Console.WriteLine("\nК О Н С О Л Ь Н О Е   П Р И Л О Ж Е Н И Е   К А Л Ь К У Л Я Т О Р\nversion 0.1   developed by andrey cherepanov\n");
             Console.WriteLine("Результат: ");
             if (c == 1) Console.WriteLine(a + " + " + b + " = " + (a + b) + "\n");
             if (c == 2) Console.WriteLine(a + " – " + b + " = " + (a - b) + "\n");
